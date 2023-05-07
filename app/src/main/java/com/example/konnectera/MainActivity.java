af79611a66d210b87952c3d845d9362232736ebf
Jeffrey Rowlings <71753362+itsrawlinz-jeff@users.noreply.github.com>
2023-05-07 09:05:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/konnectera/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.konnectera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView= findViewById(R.id.webview);
+        webView.loadUrl("http://konnectera.bsoundsentertainment.com/")
+
+    }
+}
